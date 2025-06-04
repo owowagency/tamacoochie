0da36a381f83ad0330697e8d9204eec5311717f2
Mees <git@amees.me>
2025-06-04 19:26:21 +0200

    Updating rates

--- src/game.js
+++ src/game.js
@@ -11,7 +11,7 @@ const config = {
   defaultSleepLevel: 24,
   defaultPlayLevel: 24,
   sceneTimeOut: 10 * 1000, // 10 seconds
-  speed: 1000 * 60, // 1 tick per minute
+  speed: 1000 * 30, // 1 tick per minute
   // speed: 3600 * 24 * 1000, // 1 tick per hour in ms
   debugSpeed: 1000, // 1 tick per second
 };
